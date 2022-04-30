e6e6aabec7c2c991dcb11323f72edb8add3225e7
juniorCoderKrithika <72335062+juniorCoderKrithika@users.noreply.github.com>
2022-04-30 15:34:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//https://teachablemachine.withgoogle.com/models/bqCpj-Qu-/model.json
+prediction_1="";
+prediction_2="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach(camera);
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log("ml5 version - ",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/bqCpj-Qu-/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("Model is Initialized")
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data1="the first prediction is " + prediction_1;
+    speak_data2=" and the second prediction is " + prediction_2;
+    utterThis=new SpeechSynthesisUtterance(speak_data1+speak_data2);
+    synth.speak(utterThis);
+}
